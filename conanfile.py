c8840f66780d7f334212461e092b2ef9acb73f84
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:24 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile, tools, os
 class BoostXpressiveConan(ConanFile):
     name = "Boost.Xpressive"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/xpressive"
+    url = "https://github.com/bincrafters/conan-boost-xpressive"
+    source_url = "https://github.com/boostorg/xpressive"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "xpressive"
@@ -34,7 +34,7 @@ class BoostXpressiveConan(ConanFile):
 
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
+                 .format(self.version, self.source_url))
 
     def package(self):
         include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
