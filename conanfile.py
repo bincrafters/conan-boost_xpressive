2f6bb38608a7cd6847df5a6ad73d1e959b0309f2
Jerry W <jerrywiltse@gmail.com>
2017-08-20 00:43:57 -0400

    enabled short paths

--- conanfile.py
+++ conanfile.py
@@ -3,6 +3,7 @@ from conans import ConanFile, tools, os
 class BoostXpressiveConan(ConanFile):
     name = "Boost.Xpressive"
     version = "1.64.0"
+    short_paths = True
     url = "https://github.com/bincrafters/conan-boost-xpressive"
     source_url = "https://github.com/boostorg/xpressive"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
@@ -34,7 +35,7 @@ class BoostXpressiveConan(ConanFile):
 
     def source(self):
         for lib_short_name in self.lib_short_names:
-            self.run("git clone --depth=50 --branch=boost-{0} https://github.com/boostorg/{1}.git"
+            self.run("git clone --depth=1 --branch=boost-{0} https://github.com/boostorg/{1}.git"
                      .format(self.version, lib_short_name)) 
 
     def package(self):
