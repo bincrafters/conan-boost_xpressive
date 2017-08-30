fd3bcbabfcef16d33d9a5c540e5a2810cd8c9852
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-30 12:28:35 -0400

    Fixed source methods and a few package methods

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,6 @@ class BoostXpressiveConan(ConanFile):
     version = "1.64.0"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-xpressive"
-    source_url = "https://github.com/boostorg/xpressive"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["xpressive"]
@@ -34,9 +33,12 @@ class BoostXpressiveConan(ConanFile):
                       #assert1 config0 conversion5 core2 exception5 fusion5 integer3 iterator5 lexical_cast8 mpl5 numeric~conversion6 optional5 preprocessor0 proto8 range7 smart_ptr4 static_assert1 throw_exception2 type_traits3 typeof5 utility5
 
     def source(self):
+        boostorg_github = "https://github.com/boostorg"
+        archive_name = "boost-" + self.version  
         for lib_short_name in self.lib_short_names:
-            self.run("git clone --depth=1 --branch=boost-{0} https://github.com/boostorg/{1}.git"
-                     .format(self.version, lib_short_name)) 
+            tools.get("{0}/{1}/archive/{2}.tar.gz"
+                .format(boostorg_github, lib_short_name, archive_name))
+            os.rename(lib_short_name + "-" + archive_name, lib_short_name)
 
     def package(self):
         for lib_short_name in self.lib_short_names:
