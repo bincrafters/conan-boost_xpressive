9713f75f97da5cb14565c0ed6c04d8b84e909070
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:12:32 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostXpressiveConan(ConanFile):
     name = "boost_xpressive"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_xpressive"
-    
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["xpressive"]
     is_header_only = True
 
